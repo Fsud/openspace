48c6eb934996e3fb0c099f66800f1047c23a0ca6
Fsud <gfanle111@gmail.com>
2024-04-22 19:19:12 +0800

    W1D5 TokenBank二次提交

--- W1D4/BigBank.sol
+++ W1D4/BigBank.sol
@@ -3,18 +3,33 @@
 pragma solidity ^0.8.0;
 
 import './Bank.sol';
-import './Ownable.sol';
 
-contract BigBank is Bank, Ownable {
+contract Ownable{
 
-    //校验最小存入modifier
+    event Received(address from, address to, uint amount);
+
+    receive() external payable{
+        emit Received(msg.sender, address(this), msg.value);
+    }
+
+    function withdraw(address payable bank) public{
+        BigBank(bank).withdraw();
+    }
+
+}
+
+
+contract BigBank is Bank {
+
+
     modifier minValue(uint value){
         require(msg.value >= value,"value too small");
         _;
     }
 
-    function withdraw() public onlyOwner override{
-        (payable(owner)).transfer(address(this).balance);
+    function withdraw() public override{
+        require(msg.sender == admin,"not admin");
+        (payable(admin)).transfer(address(this).balance);
     }
 
     function deposit() public payable minValue(0.001 ether) override{ 
@@ -23,6 +38,11 @@ contract BigBank is Bank, Ownable {
 
     receive() external minValue(0.001 ether) payable override{
         deposit();
+    }
+
+    function changeOwner(address newOwner) public {
+        require(msg.sender == admin,"not admin");
+        admin = newOwner;
     }
 
 }
---
+++ W1D5/readme.md
@@ -0,0 +1,8 @@
+## 调用erc20的approve
+[](WX20240422-190054@2x.png)
+
+## 调用TokenBank的deposit
+[](WX20240422-190359@2x.png)
+
+## 调用TokenBank的withdraw
+[](WX20240422-191254@2x.png)
